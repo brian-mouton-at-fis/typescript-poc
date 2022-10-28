b05b5e268859766d54802c14746ed948df6eca49
brian-mouton-at-fis <brian.mouton@fisglobal.com>
2022-10-28 16:51:08 +0000

    Completed project

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
-    selector: 'courses'
+    selector: 'courses',
+    template: `
+        <h2>{{ title }}</h2>
+        <h3>{{ courses.length }} Authors</h3>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    `
 })
 export class CoursesComponent {
+    title = "List of courses";
+    courses;
+    
+    constructor(service: CoursesService) {
+        this.courses = service.getCourses();
+    }
 
 }
